f742867a562ae40a9a13a25a2a03f66e64eb8011
Chris Were <chris@mozzler.com.au>
2019-01-02 06:42:13 +0000

    Make RbacFilter execute oauth, then execute RBAC checks

--- filters/RbacFilter.php
+++ filters/RbacFilter.php
@@ -2,24 +2,48 @@
 namespace mozzler\rbac\filters;
 
 use Yii;
-use yii\base\ActionFilter;
+//use yii\base\ActionFilter;
+use yii\base\Controller;
 use yii\web\ForbiddenHttpException;
+use filsh\yii2\oauth2server\exceptions\HttpException;
 
-class RbacFilter extends ActionFilter
+class RbacFilter extends \yii\base\Behavior
 {
-	
-	public function beforeAction($action)
+
+	public function events()
     {
-	    if (!\Yii::$app->rbac->canAccessAction($action))
+        return [
+        	Controller::EVENT_BEFORE_ACTION => 'beforeAction'
+        ];
+    }
+    
+    public function beforeAction($event)
+    {   
+	    // Check if OAuth2 token is valid
+	    $response = \Yii::$app->getModule('oauth2')->getServer()->getResponse();
+
+        $isValid = true;
+        if($response !== null) {
+            $isValid = $response->isInformational() || $response->isSuccessful() || $response->isRedirection();
+        }
+        if(!$isValid) {
+            throw new HttpException($response->getStatusCode(), $this->getErrorMessage($response), $response->getParameter('error_uri'));
+        }
+	    
+	    // Check if RBAC permissions permit access to this controller
+	    if (!\Yii::$app->rbac->canAccessAction($event->action))
 	    {
 		    throw new ForbiddenHttpException("No permission to access this page");
 	    }
-
-        return parent::beforeAction($action);
     }
     
-    public static function rbac() {
-	    return [];
+    protected function getErrorMessage(\OAuth2\Response $response)
+    {
+        $message = \Yii::$app->getModule('oauth2')->t('common', $response->getParameter('error_description'));
+        if($message === null) {
+            $message = \Yii::$app->getModule('oauth2')->t('common', 'An internal server error occurred.');
+        }
+        return $message;
     }
     
 }
