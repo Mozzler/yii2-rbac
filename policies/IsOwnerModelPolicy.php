78384a4eeed9932d013e437966dafd3bdca180e1
Chris Were <chris@mozzler.com.au>
2019-02-14 12:51:49 +1030

    Wrap relation in MongoId if required

--- policies/IsOwnerModelPolicy.php
+++ policies/IsOwnerModelPolicy.php
@@ -2,6 +2,7 @@
 namespace mozzler\rbac\policies;
 
 use mozzler\rbac\policies\BasePolicy;
+use mozzler\base\components\Tools;
 
 /**
  * Policy to generate a database filter restricting access to
@@ -12,15 +13,21 @@ class IsOwnerModelPolicy extends BasePolicy {
 	public $idAttribute = '_id';
 	public $ownerAttribute = 'createdUserId';
 	
-	public function run() {
+	public function run($params=[]) {
 		$user = \Yii::$app->user->getIdentity();
 		
 		if (!$user) {
 			return false;
 		}
-		
+
+		$match = $user->__get($this->idAttribute);
+
+		if (substr($this->idAttribute,-2) == 'Id') {
+			$match = Tools::ensureId($match);
+		}
+
 		return [
-			$this->ownerAttribute => strval($user->__get($this->idAttribute))
+			$this->ownerAttribute => $match
 		];
 	}
 	
